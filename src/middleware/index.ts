a0f0484cfc69bd58f32c99b47b0e9c77e301957a
José Gamaliel Rivera Ibarra <j0s3.r1v3r@gmail.com>
2021-06-18 11:47:54 -0700

    Export jwt_validator_middleware

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,2 +1,2 @@
 export { openApiErrorHandler } from "./OpenApiErrorHandler";
-export { admin_token_validator_middleware, token_validator_middleware } from "./TokenValidator";
+export { admin_token_validator_middleware, token_validator_middleware, PublicKeyInfo, jwt_validator_middleware } from "./TokenValidator";
